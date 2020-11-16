d57b98acc972f0f3ddb9c215663cc9b8c2c0b02b
fintank <finapptank@gmail.com>
2020-11-15 23:08:15 -0500

    corrected file name

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const apiRoutes = require('./routes/ApiRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 
